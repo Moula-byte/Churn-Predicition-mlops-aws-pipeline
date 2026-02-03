0d97107cecd6a1e72bf01fc9d0a22b0197501179
Moula Sab Pinjar <mpinjar82@gmail.com>
2026-02-03 19:17:12 +0530

    metrices extracted from s3

--- src/ingestion/Extract_metrics_from_s3.py
+++ src/ingestion/Extract_metrics_from_s3.py
@@ -2,23 +2,41 @@ import boto3
 import tarfile
 import io
 import json
+from src.logger import setup_logger
 
+logger=setup_logger("Extract_metrics_from_s3")
 # --- Configuration ---
 BUCKET = "ml-prod-pipeline"
 REGION = "ap-south-1"
 
 # The path to the tarball SageMaker created
 # Replace [JOB_NAME] with your actual job name from logs
-MODEL_TAR_KEY = "s3://ml-prod-pipeline/models/churn/xgboost/version=1/sagemaker-scikit-learn-2026-02-02-10-56-37-905/output/model.tar.gz"
-
+MODEL_TAR_KEY = "models/churn/xgboost/version=1/sagemaker-scikit-learn-2026-02-02-10-56-37-905/output/model.tar.gz"
+logger.info(F"Model Registry Path: {MODEL_TAR_KEY}")
 # The new location for the extracted metrics
-DESTINATION_METRICS_KEY = "s3://ml-prod-pipeline/models/churn/xgboost/version=1/sagemaker-scikit-learn-2026-02-02-10-56-37-905/output/metrics/metrics.json"
+DESTINATION_METRICS_KEY = "models/churn/xgboost/version=1/sagemaker-scikit-learn-2026-02-02-10-56-37-905/output/metrics/metrics.json"
+logger.info(f"destination Path is : {DESTINATION_METRICS_KEY}")
+
+
+''' 
+PREFIX = "models/churn/xgboost/version=1/"
+
+s3 = boto3.client('s3')
+response = s3.list_objects_v2(Bucket=BUCKET, Prefix=PREFIX)
+
+if 'Contents' in response:
+    print("Found these files in your bucket:")
+    for obj in response['Contents']:
+        print(f"{obj['Key']}")
+else:
+    print("No files found. Check your BUCKET name and PREFIX.")
+'''
 
 
 def extract_and_upload_metrics():
     s3 = boto3.client('s3', region_name=REGION)
 
-    print(f"Downloading {MODEL_TAR_KEY}...")
+    logger.info(f"Downloading {MODEL_TAR_KEY}...")
 
     # 1. Get the tarball from S3 into memory
     response = s3.get_object(Bucket=BUCKET, Key=MODEL_TAR_KEY)
@@ -31,8 +49,7 @@ def extract_and_upload_metrics():
             metrics_file = tar.extractfile("metrics.json")
             if metrics_file:
                 metrics_content = metrics_file.read()
-                print("Found metrics.json inside tarball.")
-
+                logger.info("Found metrics.json inside tarball.")
                 # 3. Upload the content back to the new S3 location
                 s3.put_object(
                     Bucket=BUCKET,
@@ -40,11 +57,11 @@ def extract_and_upload_metrics():
                     Body=metrics_content,
                     ContentType='application/json'
                 )
-                print(f"🚀 Successfully uploaded to: s3://{BUCKET}/{DESTINATION_METRICS_KEY}")
+                logger.info(f"Successfully uploaded to: s3://{BUCKET}/{DESTINATION_METRICS_KEY}")
             else:
-                print("❌ metrics.json not found in the tarball!")
+                logger.info("metrics.json not found in the tarball!")
         except KeyError:
-            print("❌ Could not find metrics.json in the archive. Check your train.py save logic.")
+            logger.error("Could not find metrics.json in the archive. Check your train.py save logic.")
 
 
 if __name__ == "__main__":
